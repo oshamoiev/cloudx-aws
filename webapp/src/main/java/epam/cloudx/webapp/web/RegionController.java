af72d24a84a343fcefe6c3e8109b2ef0f4ef124e
Oleksandr Shamoiev <oleksandr_shamoiev@epam.com>
2022-09-12 21:39:43 +0300

    Use EC2MetadataUtils to return region and AZ response

--- webapp/src/main/java/epam/cloudx/webapp/web/RegionController.java
+++ webapp/src/main/java/epam/cloudx/webapp/web/RegionController.java
@@ -1,17 +1,29 @@
 package epam.cloudx.webapp.web;
 
+import com.amazonaws.util.EC2MetadataUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("region")
 public class RegionController {
-    @GetMapping("name")
+
+    @GetMapping("test")
     public Map<String, String> getRegionName() {
-        return Map.of("regionName", "test-name");
+        return Map.of("region", "test-name");
+    }
+
+    @GetMapping("name-az")
+    public Map<String, String> getRegionAndAZ() {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("region", EC2MetadataUtils.getEC2InstanceRegion());
+        response.put("az", EC2MetadataUtils.getAvailabilityZone());
+
+        return response;
     }
 }
 
