bbf0034c98cab46c5595046119b49e41bacd1f08
Oleksandr Shamoiev <oleksandr_shamoiev@epam.com>
2022-10-19 21:12:19 +0300

    Add SQS and SNS client beans

--- webapp/src/main/java/epam/cloudx/webapp/config/AwsConfiguration.java
+++ webapp/src/main/java/epam/cloudx/webapp/config/AwsConfiguration.java
@@ -3,6 +3,10 @@ package epam.cloudx.webapp.config;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,8 +16,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AwsConfiguration {
 
     @Bean
-    public AmazonS3 getAmazonS3() {
+    public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+    @Bean
+    public AmazonSQS amazonSQS() {
+        return AmazonSQSClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+    @Bean
+    public AmazonSNS amazonSNS(){
+        return AmazonSNSClientBuilder.standard()
                 .withRegion(Regions.US_EAST_1)
                 .build();
     }
